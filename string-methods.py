f0a36df3ddf540c1fde6cf2ddfc649a7ae7912ef
arianna-gaskins <72282351+arianna-gaskins@users.noreply.github.com>
2021-05-24 11:42:27 -0400

    Update string-methods.py
    Add find(), replace(), translate()

--- string-methods.py
+++ string-methods.py
@@ -50,6 +50,50 @@ print(hello.count('ll'))
 hello = 'heLLo'
 # prints 1 (changes to lowercase first to allow proper count)
 print(hello.lower().count('ll'))
+      
+# find() allows you to search for characters/substrings within a string
+# it returns the lowest index for the characters in the string 
+# and if the characters are not found it returns -1
+hello = 'hello'
+# prints 3
+print(hello.find('ll')
+
+# you can use find to return whether or not the characters/substrings were found with if statements
+if (hello.find('ll') =! -1):
+      print('Characters were found!')
+else:
+      print('Characters were not found :(')
+
+# find() has second and third arguments which allow you to choose starting points
+# and ending points respectively to search through
+
+# replace() method can be used to remove/replace a character from a string
+# replace function can also be used to replace multiple characters 
+# and newlines or spaces
+# returns string without character that was removed, or with the new characters
+hello = 'hello' 
+# prints heyo
+print(hello.replace('ll', 'y'))
+
+# replace() function can also have a third argument for the number of times 
+# the replacement should occur (from left to right)
+hello = 'helllo'
+# prints heyylo
+print(hello.replace('l', 'y', 2)
+      
+# you can also use translate() to remove/replace characters in strings
+hello = 'hellao'
+# prints 'hello'
+print(hello.translate({ord('a'): None})
+      
+# to remove multiple characters, you can use translate() function and an iterator
+# Note: ord() is meant to change a string or character into its respective Unicode 
+# character and chr() does the opposite
+# uses 'translation table' which I can only assume is a built-in dictionary that you can access
+# and change the values of for your specific practice
+hello = 'hellabco'
+# prints 'hello'
+print(hello.translate({ord(i): None for i in 'abc'})
 
 # places the len() method onto the string
 # isn't only a string method but can be used for them also SEE LISTS AND TUPLES
